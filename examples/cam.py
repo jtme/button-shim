6a132429f97eafefc1570920150d70a409c80ee6
jtme <jtme@users.noreply.github.com>
2019-08-27 15:15:13 +0200

    Update cam.py

--- examples/cam.py
+++ examples/cam.py
@@ -17,7 +17,7 @@ import os
 @buttonshim.on_press(buttonshim.BUTTON_A)
 def button_a(button, pressed):
     buttonshim.set_pixel(0x94, 0x00, 0xd3)
-    os.system("echo 'hello world'")
+    os.system("lp 'hello world'")
 
 
 @buttonshim.on_press(buttonshim.BUTTON_B)
